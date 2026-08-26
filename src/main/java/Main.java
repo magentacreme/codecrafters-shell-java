bc90612cf1fdd533bcdc45d4e64a750067ca1a4a
Aditya Saha <aditksaha@gmail.com>
2026-08-26 12:25:07 +0530

    Run a program

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -2,49 +2,80 @@ import java.io.File;
 import java.util.Scanner;
 
 public class Main {
+
     public static void main(String[] args) throws Exception {
-        
+
         Scanner sc = new Scanner(System.in);
 
-        while(true){
+        while (true) {
 
             System.out.print("$ ");
 
             String input = sc.nextLine();
 
-            String parts[] = input.split(" ",2);
+            String[] parts = input.split(" ");
             String command = parts[0];
-            String argument = "";
-            if(parts.length >1){
-                argument = parts[1];
-            }
-
             switch (command) {
-                case "exit"->System.exit(0);
-                case "echo"->System.out.println(argument);
+                case "exit" -> System.exit(0);
+                case "echo" -> {
+                    for (int i = 1; i < parts.length; i++) {
+                        if (i > 1) {
+                            System.out.print(" ");
+                        }
+                        System.out.print(parts[i]);
+                    }
+                    System.out.println();
+                }
                 case "type" -> {
+                    String argument = "";
+                    if (parts.length > 1) {
+                        argument = parts[1];
+                    }
                     switch (argument) {
                         case "exit", "echo", "type" ->
-                                System.out.println(argument + " is a shell builtin");
-                        default ->{
+                                System.out.println(
+                                    argument + " is a shell builtin"
+                                );
+                        default -> {
                             String path = System.getenv("PATH");
                             String[] dir = path.split(":");
                             boolean found = false;
-                            for(String d:dir){
+                            for (String d : dir) {
                                 File file = new File(d, argument);
-                                if(file.exists() && file.canExecute()){
-                                    System.out.println(argument + " is " + file.getAbsolutePath());
+                                if (file.exists() && file.canExecute()) {
+                                    System.out.println(argument + " is " +file.getAbsolutePath());
                                     found = true;
                                     break;
                                 }
                             }
-                            if(!found){
-                                System.out.println(argument + " not found");
+                            if (!found) {
+                                System.out.println(argument + ": not found");
                             }
                         }
                     }
                 }
-                default->System.out.println(input + ": command not found");
+                default -> {
+                    String path = System.getenv("PATH");
+                    String[] dir = path.split(":");
+                    String executablePath = null;
+
+                    for (String d : dir) {
+                        File file = new File(d, command);
+                        if (file.exists() && file.canExecute()) {
+                            executablePath = file.getAbsolutePath();
+                            break;
+                        }
+                    }
+
+                    if (executablePath != null) {
+                        ProcessBuilder pb = new ProcessBuilder(parts);
+                        pb.inheritIO();
+                        Process process = pb.start();
+                        process.waitFor();
+                    } else {
+                        System.out.println(input + ": command not found");
+                    }
+                }
             }
         }
     }
