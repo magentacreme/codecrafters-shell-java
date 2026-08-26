15dd59aed81eeedfccadc95f300a66c3c0103682
Aditya Saha <aditksaha@gmail.com>
2026-08-26 12:01:48 +0530

    Implement type

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -1,23 +1,32 @@
 import java.util.Scanner;
 public class Main {
     public static void main(String[] args) throws Exception {
-        Scanner sc = new Scanner(System.in);
-        
         while(true){
+            Scanner sc = new Scanner(System.in);
             System.out.print("$ ");
 
             String input = sc.nextLine();
-            if(input.equals("exit")){
-                System.exit(0);
+
+            String parts[] = input.split(" ",2);
+            String command = parts[0];
+            String argument = "";
+            if(parts.length >1){
+                argument = parts[1];
             }
-            else if(input.startsWith("echo ")){
-                String message = input.substring(5);
-                System.out.println(message);
+
+            switch (command) {
+                case "exit"->System.exit(0);
+                case "echo"->System.out.println(argument);
+                case "type" -> {
+                    switch (argument) {
+                        case "exit", "echo", "type" ->
+                                System.out.println(argument + " is a shell builtin");
+                        default -> System.out.println(argument + ": command not found");
+                    }
+                }
+                default->System.out.println(input + ": command not found");
             }
-            else{
-                System.out.println(input+": command not found");
-            }
+            sc.close();
         }
-       
     }
 }
