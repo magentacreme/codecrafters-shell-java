e60d55319729ccea714c408e73d2baf07ffcfe01
Aditya Saha <aditksaha@gmail.com>
2026-08-26 11:32:09 +0530

    codecrafters submit [skip ci]

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -1,12 +1,13 @@
 import java.util.Scanner;
 public class Main {
     public static void main(String[] args) throws Exception {
-        System.out.print("$ ");
         Scanner sc = new Scanner(System.in);
-        for (int i = 0; i < 1; i+=0) {
+        while(true){
+            System.out.print("$ ");
+            
             String input = sc.nextLine();
             System.out.println(input+": command not found");
-            System.out.print("$ ");
         }
+       
     }
 }
