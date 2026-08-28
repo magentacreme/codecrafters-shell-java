9f44aaabc96088e17423f19834f40c78a2202854
Aditya Saha <aditksaha@gmail.com>
2026-08-28 19:02:54 +0530

    codecrafters submit [skip ci]

--- src/main/java/command/DeclareCommand.java
+++ src/main/java/command/DeclareCommand.java
@@ -1,17 +1,47 @@
 package command;
 
 import java.io.PrintStream;
+import java.util.HashMap;
 import java.util.List;
+import java.util.Map;
 
 public class DeclareCommand implements Command
 {
+    private final Map<String, String> variables = new HashMap<>();
+
     @Override
     public void execute(List<String> args, PrintStream stdout, PrintStream stderr)
     {
-        if (args.size() >= 2 && args.getFirst().equals("-p"))
+        if (!args.isEmpty() && args.getFirst().equals("-p"))
         {
-            stderr.printf("declare: %s: not found%n", args.get(1));
+            printVariable(args, stdout, stderr);
+            return;
         }
+
+        for (String argument : args)
+        {
+            int equalsIndex = argument.indexOf('=');
+            if (equalsIndex > 0)
+            {
+                variables.put(
+                        argument.substring(0, equalsIndex),
+                        argument.substring(equalsIndex + 1)
+                );
+            }
+        }
+    }
+
+    private void printVariable(List<String> args, PrintStream stdout, PrintStream stderr)
+    {
+        if (args.size() < 2 || !variables.containsKey(args.get(1)))
+        {
+            String name = args.size() < 2 ? "" : args.get(1);
+            stderr.printf("declare: %s: not found%n", name);
+            return;
+        }
+
+        String name = args.get(1);
+        stdout.printf("declare -- %s=\"%s\"%n", name, variables.get(name));
     }
 
     @Override
